b37544e705a3a6d062ab89b175dfb0e368fabbe4
cre-mer <jonasmerhej1@gmail.com>
2021-11-09 09:25:39 +0100

    responsive footer

--- packages/app/src/components/Footer/Footer.tsx
+++ packages/app/src/components/Footer/Footer.tsx
@@ -8,69 +8,65 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
 	return (
 		<StyledFooter>
-			<div style={{ height: `${theme.footer.height}px` }}/>
-			<StyledFooterInner>
-				<StyledFooterGrid>
-					<div>
-						<img loading='lazy' src={logoexpand} alt='pepemon logo' style={{maxWidth: '10em'}}/>
-					</div>
-					<div>
-						<Title as='h2' size='xxxs' weight={400} color={theme.color.white}>Start researching</Title>
-						<StyledList>
-							<li><StyledLink href='https://docs.pepemon.world/'>Docs</StyledLink></li>
-							<li><StyledLink href='https://opensea.io/collection/pepemonfactory'>Opensea</StyledLink></li>
-							<li><StyledLink href='https://docs.pepemon.world/gaming'>Pepemon: Degen Battleground</StyledLink></li>
-						</StyledList>
-					</div>
-					<div>
-						<Title as='h2' size='xxxs' weight={400} color={theme.color.white}>Start earning</Title>
-						<StyledList>
-							<li><ExternalStyledLink href='https://etherscan.io/token/0x4d2ee5dae46c86da2ff521f7657dad98834f97b8'>PPBLZ Contract</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://etherscan.io/token/0xf1f508c7c9f0d1b15a76fba564eef2d956220cf7'>PPDEX Contract</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://app.uniswap.org/#/swap?outputCurrency=0x4d2ee5dae46c86da2ff521f7657dad98834f97b8'>Buy $PPBLZ</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://app.uniswap.org/#/swap?outputCurrency=0xf1f508c7c9f0d1b15a76fba564eef2d956220cf7'>Buy $PPDEX</ExternalStyledLink></li>
-						</StyledList>
-					</div>
-
-					<div>
-						<Title as='h2' size='xxxs' weight={400} color={theme.color.white}>Get in touch</Title>
-						<StyledList>
-							<li><ExternalStyledLink href='https://twitter.com/pepemonfinance'>Twitter</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://t.me/pepemonfinance'>Telegram</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://discord.gg/R8sZwMv'>Discord</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://github.com/pepem00n'>Github</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://medium.com/@pepemonfinance'>Medium</ExternalStyledLink></li>
-							<li><ExternalStyledLink href='https://opensea.io/collection/pepemonfactory'>OpenSea</ExternalStyledLink></li>
-						</StyledList>
-					</div>
-				</StyledFooterGrid>
-				<StyledFooterLegal>
-					<hr/>
-					<StyledFooterLegalInner>
-						<StyledFooterLegalLinks>
-							<Link to='/terms-of-service'>Terms of service</Link>
-							<Link to='/privacy-policy'>Privacy policy</Link>
-						</StyledFooterLegalLinks>
+			<StyledFooterImg src={footercover}/>
+			<StyledFooterInnerWrapper>
+				<StyledFooterInner>
+					<StyledFooterGrid>
 						<div>
-							© 2021
+							<img loading='lazy' src={logoexpand} alt='pepemon logo' style={{maxWidth: '10em'}}/>
 						</div>
-					</StyledFooterLegalInner>
-				</StyledFooterLegal>
-			</StyledFooterInner>
+						<div>
+							<Title as='h2' size='xxxs' weight={400} color={theme.color.white}>Start researching</Title>
+							<StyledList>
+								<li><StyledLink href='https://docs.pepemon.world/'>Docs</StyledLink></li>
+								<li><StyledLink href='https://opensea.io/collection/pepemonfactory'>Opensea</StyledLink></li>
+								<li><StyledLink href='https://docs.pepemon.world/gaming'>Pepemon: Degen Battleground</StyledLink></li>
+							</StyledList>
+						</div>
+						<div>
+							<Title as='h2' size='xxxs' weight={400} color={theme.color.white}>Start earning</Title>
+							<StyledList>
+								<li><ExternalStyledLink href='https://etherscan.io/token/0x4d2ee5dae46c86da2ff521f7657dad98834f97b8'>PPBLZ Contract</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://etherscan.io/token/0xf1f508c7c9f0d1b15a76fba564eef2d956220cf7'>PPDEX Contract</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://app.uniswap.org/#/swap?outputCurrency=0x4d2ee5dae46c86da2ff521f7657dad98834f97b8'>Buy $PPBLZ</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://app.uniswap.org/#/swap?outputCurrency=0xf1f508c7c9f0d1b15a76fba564eef2d956220cf7'>Buy $PPDEX</ExternalStyledLink></li>
+							</StyledList>
+						</div>
+
+						<div>
+							<Title as='h2' size='xxxs' weight={400} color={theme.color.white}>Get in touch</Title>
+							<StyledList>
+								<li><ExternalStyledLink href='https://twitter.com/pepemonfinance'>Twitter</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://t.me/pepemonfinance'>Telegram</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://discord.gg/R8sZwMv'>Discord</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://github.com/pepem00n'>Github</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://medium.com/@pepemonfinance'>Medium</ExternalStyledLink></li>
+								<li><ExternalStyledLink href='https://opensea.io/collection/pepemonfactory'>OpenSea</ExternalStyledLink></li>
+							</StyledList>
+						</div>
+					</StyledFooterGrid>
+					<StyledFooterLegal>
+						<hr/>
+						<StyledFooterLegalInner>
+							<StyledFooterLegalLinks>
+								<Link to='/terms-of-service'>Terms of service</Link>
+								<Link to='/privacy-policy'>Privacy policy</Link>
+							</StyledFooterLegalLinks>
+							<div>
+								© 2021
+							</div>
+						</StyledFooterLegalInner>
+					</StyledFooterLegal>
+				</StyledFooterInner>
+			</StyledFooterInnerWrapper>
 		</StyledFooter>
 	)
 }
 
 const StyledFooter = styled.footer`
-	background-position-x: center;
-	background-position-y: 100%;
-	background-image: url(${footercover});
-	background-size: 100% auto;
-	background-repeat: no-repeat;
-	padding-left: 2em;
-	padding-right: 2em;
-	position: absolute;
 	bottom: 0;
+	position: absolute;
+	width: 100vw;
 
 	@media (min-width: ${theme.breakpoints.desktop}) {
 		margin-left: ${theme.sideBar.width}px;
@@ -78,17 +74,29 @@ const StyledFooter = styled.footer`
 	}
 `
 
+const StyledFooterImg = styled.img`
+	display: block;
+	width: 100%;
+`
+
+const StyledFooterInnerWrapper = styled.div`
+	background-color: ${theme.color.purple[800]};
+`
+
 const StyledFooterInner = styled.div`
 	margin-left: auto;
 	margin-right: auto;
 	max-width: ${theme.page.maxWidth}px;
+	padding-left: 2em;
+	padding-right: 2em;
 `
 
 const StyledFooterGrid = styled.div`
 	display: grid;
-	grid-auto-columns: 1fr;
-	grid-template-columns: repeat(4, 1fr);
+	grid-gap: 1.5em;
+	grid-template-columns: repeat(auto-fit, minmax(190px, 1fr));
 	padding-bottom: 2em;
+	padding-top: 2em;
 `
 
 const StyledList = styled.ul`
@@ -123,9 +131,13 @@ const StyledFooterLegal = styled.div`
 const StyledFooterLegalInner = styled.div`
 	align-items: center;
 	display: flex;
+	flex-wrap: wrap;
 	justify-content: space-between;
 	padding-bottom: 1.5em;
-	padding-top: 1.5em;
+
+	div {
+		margin-top: 1.5em;
+	}
 `
 
 const StyledFooterLegalLinks = styled.div`
@@ -133,11 +145,8 @@ const StyledFooterLegalLinks = styled.div`
 
 	a {
 		color: currentColor;
+		margin-right: 1.5em;
 		text-decoration: none;
-
-		&:not(:first-child) {
-			margin-left: 1.5em;
-		}
 	}
 `
 
