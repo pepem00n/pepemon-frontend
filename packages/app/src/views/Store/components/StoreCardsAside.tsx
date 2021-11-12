5e820ed9af31d2c43e40671136f820869e89ebec
cre-mer <jonasmerhej1@gmail.com>
2021-11-12 17:16:38 +0100

    fix claim card modal

--- packages/app/src/views/Store/components/StoreCardsAside.tsx
+++ packages/app/src/views/Store/components/StoreCardsAside.tsx
@@ -84,68 +84,70 @@ const StoreCardsAside: React.FC<any> = ({setSelectedCard, selectedCard: { cardId
 	}
 
 	return (
-		<StoreAside close={() => setSelectedCard("")} title="Selected Card">
-			<StyledStoreBody>
-				<Title as="h2" font={theme.font.neometric} size='m'>{cardMeta ? cardMeta.name : 'Loading card'}</Title>
-				<Spacer size="sm"/>
-				<Text as="p" font={theme.font.inter} size='s' lineHeight={1.3} color={theme.color.gray[600]}>{cardMeta && cardMeta.description}</Text>
-				<Spacer size="sm"/>
-				<img loading="lazy" src={cardMeta ? cardMeta.image : cardback_normal} alt={cardMeta ? cardMeta.name : 'Loading card'} style={{width: "100%"}}/>
-				<Spacer size='md'/>
-				<StyledPepemonCardMeta>
-					<dt>Rarity:</dt>
-					<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Rarity').value}</dd>
-				</StyledPepemonCardMeta>
-				<Spacer size='sm'/>
-				<StyledSpacer bg={theme.color.gray[100]} size={2}/>
-				<StyledPepemonCardMeta>
-					<dt>Type:</dt>
-					<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Type').value}</dd>
-				</StyledPepemonCardMeta>
-				<Spacer size='sm'/>
-				<StyledSpacer bg={theme.color.gray[100]} size={2}/>
-				<StyledPepemonCardMeta>
-					<dt>Set:</dt>
-					<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Set').value}</dd>
-				</StyledPepemonCardMeta>
-				<Spacer size='sm'/>
-				<StyledSpacer bg={theme.color.gray[100]} size={2}/>
-				<StyledPepemonCardMeta>
-					<dt>Artist:</dt>
-					<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Artist').value}</dd>
-				</StyledPepemonCardMeta>
-				<Spacer size='sm'/>
-				<StyledSpacer bg={theme.color.gray[100]} size={2}/>
-				<StyledPepemonCardMeta>
-					<dt>Price:</dt>
-					<dd>
-						<StyledPepemonCardPrice styling="alt">
-							<img loading="lazy" src={coin} alt="coin"/>
-							{cardPrice ? `${priceOfCard} ${chainId === 56 ? 'BNB' : 'PPDEX'}` : 'loading'}
-						</StyledPepemonCardPrice>
-					</dd>
-				</StyledPepemonCardMeta>
-				<Spacer size='md'/>
-				<ExternalLink style={{ width: '100%' }} href={chainId === 137 ? `https://quickswap.exchange/#/swap?outputCurrency=${contracts.ppdex.address}` :
-				chainId === 56 ? 'https://www.binance.com/en/trade/BNB_ETH' : `https://app.uniswap.org/#/swap?outputCurrency=${contracts.ppdex.address}`}>
-					Buy {chainId === 56 ? 'BNB' : 'PPDEX'}
-				</ExternalLink>
-				<Spacer size='md'/>
-				<Button width="100%" styling="purple"
-					disabled={buttonProps().disabled}
-					onClick={buttonProps().onClick}>
-						{buttonProps().text}
-				</Button>
-				{ activeClaimModal &&
-					<StoreClaimModal
-						dismiss={() => setActiveClaimModal(false)}
-						claimButtonText={isRedeemingCard ? 'Claiming...' : 'Claim card'}
-						claimButtonClick={() => onRedeemCard(cardId, chainId === 56 ? priceOfCard.toString() : null).then(() => setTransactions(transactions + 1))}
-						claimButtonDisabled={isRedeemingCard}
-					/>
-				}
-			</StyledStoreBody>
-		</StoreAside>
+		<>
+			<StoreAside close={() => setSelectedCard("")} title="Selected Card">
+				<StyledStoreBody>
+					<Title as="h2" font={theme.font.neometric} size='m'>{cardMeta ? cardMeta.name : 'Loading card'}</Title>
+					<Spacer size="sm"/>
+					<Text as="p" font={theme.font.inter} size='s' lineHeight={1.3} color={theme.color.gray[600]}>{cardMeta && cardMeta.description}</Text>
+					<Spacer size="sm"/>
+					<img loading="lazy" src={cardMeta ? cardMeta.image : cardback_normal} alt={cardMeta ? cardMeta.name : 'Loading card'} style={{width: "100%"}}/>
+					<Spacer size='md'/>
+					<StyledPepemonCardMeta>
+						<dt>Rarity:</dt>
+						<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Rarity').value}</dd>
+					</StyledPepemonCardMeta>
+					<Spacer size='sm'/>
+					<StyledSpacer bg={theme.color.gray[100]} size={2}/>
+					<StyledPepemonCardMeta>
+						<dt>Type:</dt>
+						<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Type').value}</dd>
+					</StyledPepemonCardMeta>
+					<Spacer size='sm'/>
+					<StyledSpacer bg={theme.color.gray[100]} size={2}/>
+					<StyledPepemonCardMeta>
+						<dt>Set:</dt>
+						<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Set').value}</dd>
+					</StyledPepemonCardMeta>
+					<Spacer size='sm'/>
+					<StyledSpacer bg={theme.color.gray[100]} size={2}/>
+					<StyledPepemonCardMeta>
+						<dt>Artist:</dt>
+						<dd>{cardMeta && cardMeta.attributes.find((trait) => trait.trait_type === 'Artist').value}</dd>
+					</StyledPepemonCardMeta>
+					<Spacer size='sm'/>
+					<StyledSpacer bg={theme.color.gray[100]} size={2}/>
+					<StyledPepemonCardMeta>
+						<dt>Price:</dt>
+						<dd>
+							<StyledPepemonCardPrice styling="alt">
+								<img loading="lazy" src={coin} alt="coin"/>
+								{cardPrice ? `${priceOfCard} ${chainId === 56 ? 'BNB' : 'PPDEX'}` : 'loading'}
+							</StyledPepemonCardPrice>
+						</dd>
+					</StyledPepemonCardMeta>
+					<Spacer size='md'/>
+					<ExternalLink style={{ width: '100%' }} href={chainId === 137 ? `https://quickswap.exchange/#/swap?outputCurrency=${contracts.ppdex.address}` :
+					chainId === 56 ? 'https://www.binance.com/en/trade/BNB_ETH' : `https://app.uniswap.org/#/swap?outputCurrency=${contracts.ppdex.address}`}>
+						Buy {chainId === 56 ? 'BNB' : 'PPDEX'}
+					</ExternalLink>
+					<Spacer size='md'/>
+					<Button width="100%" styling="purple"
+						disabled={buttonProps().disabled}
+						onClick={buttonProps().onClick}>
+							{buttonProps().text}
+					</Button>
+				</StyledStoreBody>
+			</StoreAside>
+			{ activeClaimModal &&
+				<StoreClaimModal
+				dismiss={() => setActiveClaimModal(false)}
+				claimButtonText={isRedeemingCard ? 'Claiming...' : 'Claim card'}
+				claimButtonClick={() => onRedeemCard(cardId, chainId === 56 ? priceOfCard.toString() : null).then(() => setTransactions(transactions + 1))}
+				claimButtonDisabled={isRedeemingCard}
+				/>
+			}
+		</>
 	)
 }
 
